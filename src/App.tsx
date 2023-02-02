729d8e2d16e69e6a4aef463732692d42b58240e4
Samiddha Palitt <samiddhapalit@gmail.com>
2023-02-02 13:09:51 +0530

    Finished task

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ class App extends Component<{}, IState> {
         <div className="App-content">
           <button className="btn btn-primary Stream-button"
             // when button is click, our react app tries to request
-            // new data from the server.
+            // new data from the server
             // As part of your task, update the getDataFromServer() function
             // to keep requesting the data every 100ms until the app is closed
             // or the server does not return anymore data.
